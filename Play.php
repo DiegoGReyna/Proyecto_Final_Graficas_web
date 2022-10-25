fd6f43af8b0907744be53d5d194282b2fb2da176
AnaPatricia02 <98660028+AnaPatricia02@users.noreply.github.com>
2022-10-24 21:01:56 -0500

    icons pantalla

--- Play.php
+++ Play.php
@@ -9,7 +9,8 @@
 
     <!-- <script type="text/javascript" src="js/threeJS/three.module.js"></script> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/stylePlay.css">
+    <!-- <link rel="stylesheet" href="css/stylePlay.css"> -->
+    <link rel="stylesheet" href="css/StylePlays.css">
     <title>Document</title>
     <script type="module" src="js/CanvasThreeJS.js"></script>
 
@@ -20,15 +21,32 @@
         <h2 id="cargando">Cargando...</h2>
     </div>
 
-    <div class="ContainerPlayGame" id="scene-section" />
+    <div class="ContainerPlayGame" id="scene-section" style="position: relative">
     <button class="BtnPause" onclick="window.location.href = 'Pause.html';"><img src="img/Pause.svg" alt="" width="40px" height="40px"></button>
+    </div>
+        <div class="ContainerWidget" style="position: absolute" top="470px" left="12px"> <!-- icons pantalla -->
+                    <table>
+                        <tr>
+                            <th><img src="img/esfera_Naranja2.png" width="65px" height="50px"></th>
+                            <th><img src="img/esfera_Roja2.png" width="65px" height="50px"></th>
+                            <th><img src="img/piramide2.png" width="65px" height="50px"></th>
+                            <th><img src="img/barril.png" width="70px" height="50px"></th>
+                        </tr>
+                        <tr>
+                            <td>X 3</td>
+                            <td>5</td>
+                            <td>2</td>
+                            <td>30 \ 10 </td>
+                        </tr>
+                    </table>
+        </div>
+   
     <!--SCRIPTS ventana cargando-->
-    <script>
-        var loader = document.getElementById('contenedor_carga');
-        window.addEventListener("load", function() {
-            loader.style.display = "none";
-        })
-    </script>
+        <script>
+            var loader = document.getElementById('contenedor_carga');
+            window.addEventListener("load", function() {
+                loader.style.display = "none";
+            })
+        </script>    
 </body>
-
 </html>
